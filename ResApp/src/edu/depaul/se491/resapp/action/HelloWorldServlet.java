2768215cc04ace8d4020a7f91ce9f0939fabe2d3
Malik <malik491@users.noreply.github.com>
2016-01-28 18:52:50 -0600

    commit

---
+++ ResApp/src/edu/depaul/se491/resapp/action/HelloWorldServlet.java
@@ -0,0 +1,22 @@
+package edu.depaul.se491.resapp.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+}
